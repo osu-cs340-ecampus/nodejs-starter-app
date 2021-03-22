57642cd5780c48ca43846de39eaf5f699582f298
George Kochera <kocherag@oregonstate.edu>
2021-03-21 18:47:13 -0700

    sanitized db-connector

--- Step 4 - Dynamically Displaying Data/database/db-connector.js
+++ Step 4 - Dynamically Displaying Data/database/db-connector.js
@@ -5,9 +5,9 @@ var mysql = require('mysql')
 var pool = mysql.createPool({
     connectionLimit : 10,
     host            : 'classmysql.engr.oregonstate.edu',
-    user            : 'cs340_kocherag',
-    password        : '2845',
-    database        : 'cs340_kocherag'
+    user            : '*',
+    password        : '*',
+    database        : '*'
 })
 
 // Export it for use in our applicaiton
